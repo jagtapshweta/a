db8c2ff5808d6cfca2d5a3be3cde6483f554e161
Shweta <jagtapshweta26@gmail.com>
2025-05-20 17:51:02 +0530

    timpass2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.lang.*;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("Hello Shweta");
+        System.out.println("Prakash Jagtap.");
     }
 }
 
